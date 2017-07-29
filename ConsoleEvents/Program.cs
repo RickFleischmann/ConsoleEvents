5ba5cbd42777bbab0f3c6760c072d789fe2000cf
RickFleischmann <rickf@careercruising.com>
2017-07-29 10:07:32 -0400

    ha3

--- ConsoleEvents/Program.cs
+++ ConsoleEvents/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleEventTutorial
             Adder a = new Adder();
             dgPointer pAdder = new dgPointer(a.Add);
             int iAnswer = pAdder(4, 3);
-            Console.WriteLine("iAnswer = {0} ha!", iAnswer);
+            Console.WriteLine("iAnswer = {0} ha3!", iAnswer);
             Console.ReadLine();
             Console.ReadLine();
             // Returns “iAnswer = 7”

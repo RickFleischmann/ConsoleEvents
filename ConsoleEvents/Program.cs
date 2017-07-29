bdb1a04f2dd894b2def0f41c56dd16551da3599a
RickFleischmann <rickf@careercruising.com>
2017-07-29 08:21:14 -0400

    third

--- ConsoleEvents/Program.cs
+++ ConsoleEvents/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleEvents
+namespace ConsoleEventTutorial
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public delegate int dgPointer(int a, int b);
+        static void Main()
         {
+
+            Adder a = new Adder();
+            dgPointer pAdder = new dgPointer(a.Add);
+            int iAnswer = pAdder(4, 3);
+            Console.WriteLine("iAnswer = {0}", iAnswer);
+            Console.ReadLine();
+            Console.ReadLine();
+            // Returns “iAnswer = 7”
         }
+    }
+
+    public class Adder
+    {
+        public int Add(int x, int y)
+        { return x + y; }
     }
 }
